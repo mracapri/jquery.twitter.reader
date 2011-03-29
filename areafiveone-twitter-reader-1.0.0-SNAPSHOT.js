0e7790b7c5c66347fe407accdcabf4cb9d3f2619
Mario Rivera Angeles <mra_capri@msn.com>
2011-03-29 12:26:56 -0600

    Validacion de parametros de entrada para el plugin: titterUser, timeRefresh y css

--- areafiveone-twitter-reader-1.0.0-SNAPSHOT.js
+++ areafiveone-twitter-reader-1.0.0-SNAPSHOT.js
@@ -31,11 +31,20 @@
  */
 (function($) {
 
-	var ONE 	= 1;
-	var COMMAND = 'command';
-	var CSS 	= 'css';
-	var TWITTER = "http://twitter.com/statuses/user_timeline/";
-	var FORMAT 	= ".json";
+	var EMPTY 		= " ";
+	var ONE 		= 1;
+	var TWITTER 		= "http://twitter.com/statuses/user_timeline/";
+	var FORMAT 		= ".json";
+	var FIRST_ELEMENT 	= 0;
+	var DAY 		= 0;	
+	var TAG_SPAN		= "<span/>";
+
+	var COMMAND 		= 'command';
+	var CSS 		= 'css';
+	var TWITTER_USER	= 'twitterUser';
+	var TIME_REFRESH	= 'timeRefresh';
+
+	var imageAvatar = "";
 	
 	/**
 	 *  Metodos principales
@@ -46,12 +55,15 @@
 		 * @param {array} [options] Recibe el arreglo de opciones del plugin
 		 */		
 		init : function(options){
-			$.getJSON(TWITTER + 'diabloguardian' + FORMAT + "?count=10&callback=?", function(data){
-				$.each(data, function(){
-					debug(data);
+			$.getJSON(TWITTER + options[TWITTER_USER] + FORMAT + "?count=10&callback=?", function(data){
+				$.each(data, function(key, value){
+					if(key == FIRST_ELEMENT){
+						imageAvatar = value.user['profile_image_url'];
+					}
+					var twit = $(TAG_SPAN).text(value.text + EMPTY + $.fn.twitterReader.relativeTime(value.created_at));
 				});
 			});
-			debug("Se invoco el metodo init");
+			if($.isFunction(options.result)){options.result("out");}
 		}
 	};
 
@@ -61,43 +73,41 @@
 	 * @return {object} 
 	 */
 	$.fn.twitterReader = function(options, result) {
-		// Permite crear un solo elemento
-		if($(this).size() == ONE){
-			// inicializacion de opciones del plugin antes de contruir el plugin
-			var opts = $.fn.twitterReader.defaults;
+
+
+		// inicializacion de opciones del plugin antes de contruir el plugin
+		var opts = $.fn.twitterReader.defaults;
 			
-			// Verifica si la funcion ha sido definida
-			if($.isFunction(options)){
-				result = options;
-				options = {};
-				opts = $.extend({}, opts, {"result":result});
-			}
+		// Verifica si la funcion ha sido definida
+		if($.isFunction(options)){
+			result = options;
+			options = {};
+			opts = $.extend({}, opts, {"result":result});
+		}else{
+			opts = $.extend({}, options, {"result":result});
+		}
 			
-			// verifica si las opciones vienen en closure, sino
-			// le asigna valores por default
-			var _options = !options ? opts : options;
-			if(_options.hasOwnProperty(COMMAND) && !_options.hasOwnProperty(CSS)){
-				_options = $.extend({}, opts.css, opts.result);
-			}else if(!_options.hasOwnProperty(COMMAND) && _options.hasOwnProperty(CSS)){
-				_options = $.extend({}, opts.command, opts.result);
-			}else if(!_options.hasOwnProperty(COMMAND) && !_options.hasOwnProperty(CSS)){
-				_options = $.extend({}, opts);
+		// verifica si las opciones vienen en closure, sino
+		// le asigna valores por default
+		var _options = !options ? opts : options;
+
+		if(_options.hasOwnProperty(TWITTER_USER)){
+			if(_options.hasOwnProperty(TIME_REFRESH) && !_options.hasOwnProperty(CSS)){
+				_options = $.extend({}, _options, {css : opts[CSS]}, {result : opts.result});
+			}else if(!_options.hasOwnProperty(TIME_REFRESH) && _options.hasOwnProperty(CSS)){
+				_options = $.extend({}, _options, {'timeRefresh' : opts[TIME_REFRESH]}, {result : opts.result});
 			}else{
-				_options = opts;
+				_options = $.extend({}, _options, opts);
 			}
-			
+
 			// itera y reformatea cada elemento que coincide con el elemento
 			return this.each(function() {
-				// Ejecuta los comandos
-				if((typeof(_options.command) === 'string') && methods[_options.command]) {
-					return methods[_options.command].apply($(this), [_options]);
-				}else{
-					return methods['init'].apply($(this), [_options]);
-				}
-			});		
+				return methods['init'].apply($(this), [_options]);
+			});
 		}else{
-			$(this).text("Plugin require only ONE element!");
+			$(this).text("Plugin require twitter user");
 		}
+
 	};	
 	
 	/**
@@ -117,12 +127,52 @@
 	$.fn.twitterReader.buildUi = function() {
 		return "";
 	};
+
+	/**
+	 * Establece el tiempo relativo del twit
+	 * @return {string} HTML de la interfaz de usuario
+	 */
+	$.fn.twitterReader.relativeTime = function(timeValue) {
+
+		var MONTH	= 1;
+		var YEAR 	= 5;
+		var HOUR 	= 3;
+
+		var timeValues = timeValue.split(EMPTY);
+		timeValue = timeValues[DAY] + EMPTY + timeValues[MONTH] + ", " + timeValues[YEAR] + EMPTY + timeValues[HOUR];
+		var parsedDate = Date.parse(timeValue);
+		var relativeTo = (arguments.length > ONE) ? arguments[ONE] : new Date();
+		var delta = parseInt((relativeTo.getTime() - parsedDate) / 1000);
+		delta = delta + (relativeTo.getTimezoneOffset() * 60);
+
+		var relativeTime = '';
+		if (delta < 60) {
+			relativeTime = 'a minute ago';
+		} else if(delta < 120) {
+			relativeTime = 'couple of minutes ago';
+		} else if(delta < (45*60)) {
+	    		relativeTime = (parseInt(delta / 60)).toString() + ' minutes ago';
+		} else if(delta < (90*60)) {
+	    		relativeTime = 'an hour ago';
+	  	} else if(delta < (24*60*60)) {
+	    		relativeTime = '' + (parseInt(delta / 3600)).toString() + ' hours ago';
+	  	} else if(delta < (48*60*60)) {
+	    		relativeTime = '1 day ago';
+	  	} else {
+	    		relativeTime = (parseInt(delta / 86400)).toString() + ' days ago';
+	  	}
+		return relativeTime;
+	}
+
+
+
 	
 	/**
 	 * Establece defaults del plugin
 	 */
 	$.fn.twitterReader.defaults = {
-		command: 'init'
+		timeRefresh: 60,
+		css:{valor:1}
 	};
 
 })(jQuery);
