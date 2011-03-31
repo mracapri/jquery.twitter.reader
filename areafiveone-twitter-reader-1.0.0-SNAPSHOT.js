965742b50c0575b5bc92b0ef9bd84978ccf2b38a
Mario Rivera Angeles <mra_capri@msn.com>
2011-03-30 23:41:56 -0600

    Liberada primera version del plugin

--- areafiveone-twitter-reader-1.0.0-SNAPSHOT.js
+++ areafiveone-twitter-reader-1.0.0-SNAPSHOT.js
@@ -1,6 +1,6 @@
 // The MIT License
 // 
-// Copyright (c) 2011 Valle del Bit
+// Copyright (c) 2011 Valle del Bit - Area 51
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,7 @@
 // THE SOFTWARE.
 
 
-// Autor: 	Mario Rivera Angeles (lis.mario.rivera@gmail.com)
+// Autor: 		Mario Rivera Angeles (lis.mario.rivera@gmail.com)
 // Twitter:  	twitter.com/mra_capri
 // Objetivo: 	Plugin que incrusta una componente de lectura de Twitter para RSS
 // Version: 	1.0.0-SNAPSHOT
@@ -31,24 +31,26 @@
  */
 (function($) {
 
-	var EMPTY 		= " ";
-	var ONE 		= 1;
-	var TWITTER 		= "http://twitter.com/statuses/user_timeline/";
-	var FORMAT 		= ".json";
-	var FIRST_ELEMENT 	= 0;
-	var DAY 		= 0;	
-	var TAG_SPAN		= "<span/>";
-	var TAG_UL		= "<ul/>";
-	var TAG_LI		= "<ul/>";
+	var EMPTY 				= " ";
+	var ONE 				= 1;
+	var TWITTER 			= "http://twitter.com/statuses/user_timeline/";
+	var FORMAT 				= ".json";
+	var FIRST_ELEMENT 		= 0;
+	var DAY 				= 0;	
+	var TAG_SPAN			= "<span/>";
+	var TAG_UL				= "<ul/>";
+	var TAG_LI				= "<li/>";
 
-	var COMMAND 		= "command";
-	var CSS 		= "css";
-	var TWITTER_USER	= "twitterUser";
-	var TIME_REFRESH	= "timeRefresh";
-	var NUMBER_OF_TWITS	= "numberOfTwits";
+	var COMMAND 			= "command";
+	var CSS 				= "css";
+	var TWITTER_USER		= "twitterUser";
+	var TIME_REFRESH		= "timeRefresh";
+	var NUMBER_OF_TWITS		= "numberOfTwits";
 	var CALLBACK_FUNCTION	= "result";
+	var ESASING				= 1900;
 
 	var imageAvatar 		= "";
+	var numberOfNewTwits 	= 0;
 	var firstFeching 		= true;
 	var firstFechingValues 	= new Array();
 	var lastFechingValues 	= new Array();
@@ -94,7 +96,7 @@
 				return methods['init'].apply($(this), [_options]);
 			});		
 		}else{
-			$(this).text("Plugin require twitter user");
+			$(this).text("Plugin requiere usuario de twitter con timeline publico");
 		}
 
 	};	
@@ -110,57 +112,67 @@
 	};
 	
 	/**
-	 * Construye interfaz del usuario
+	 * Construye la interfaz del contenedor
 	 * @return {string} HTML de la interfaz de usuario
 	 */
 
 	$.fn.twitterReader.buildUi = function(options) {
+	
+		// Define el contenedor
 		var container = $(TAG_UL);
+		
+		// Carga los twits al iniciar el componente
+		$.fn.twitterReader.fetchTwits(options, function(data){
+			$.each(data, function(key, value){
+				firstFechingValues[key] = value;
+				var created = $(TAG_SPAN).text($.fn.twitterReader.relativeTime(value.created_at));
+				var textFormated = $.fn.twitterReader.convertUrlToLink(value.text);
+				$(TAG_LI).html(textFormated + EMPTY + $(created).html()).fadeIn(ESASING).appendTo(container);
+			});		
+		});
+		
+		// Cada cierto tiempo hace pull al timeline del usuario establecido
 		$(container).everyTime(options[TIME_REFRESH], function(){			
 			$.fn.twitterReader.fetchTwits(options, function(data){											
-
-				if(firstFeching){						
-					$.each(data, function(key, value){
-						firstFechingValues[key] = value;
-						$("<li/>").text(value.text).appendTo(container);
-					});
-					firstFeching = false;
-				}else{
-					debug("....last fetching...");
-					$.each(data, function(key, value){
-						lastFechingValues[key] = value;
-					});
-					
-					var firstIdTwit = firstFechingValues[0].id;
-					var numberOfNewTwits = 0;
-					// Buscamos el id en la ultima lista de valores
-					$.each(lastFechingValues, function(key, value){	
-						lastFechingValues[key] = value;
-						if(lastFechingValues[key].id == firstIdTwit){
-							numberOfNewTwits = key - 0;
-						}							
-					});
-					debug("Numero de nuevos twits: " + numberOfNewTwits);
-					
-					var indexToRefres = lastFechingValues.length - numberOfNewTwits;					
-					if(numberOfNewTwits != 0){
-						debug("Indice a actualizar: " + indexToRefres);
-						
-					}
-					
-					// Removiendo twits
-					for(var indexRefresh = indexToRefres; 
-						indexRefresh < firstFechingValues.length; indexRefresh++){
-						$(container).find("li").eq(indexRefresh).remove();						
-					}
-
+				$.each(data, function(key, value){
+					lastFechingValues[key] = value;
+				});
+				
+				// Id del prime twit para copararlo con la nueva lista
+				var firstIdTwit = firstFechingValues[0].id;					
+				// Buscamos el id en la ultima lista de valores
+				$.each(lastFechingValues, function(key, value){	
+					lastFechingValues[key] = value;
+					if(lastFechingValues[key].id == firstIdTwit){
+						numberOfNewTwits = key - 0;
+					}							
+				});
+				
+				// Obteniendo el indice para refrescar a partir de ahi
+				var indexToRefresh = lastFechingValues.length - numberOfNewTwits;
+				// Removiendo twits
+				var indexTwitsAdd = numberOfNewTwits - 1;
+				
+				// Refresca los ultimos twits en el componente
+				for(var indexRefresh = indexToRefresh; 
+					indexRefresh < firstFechingValues.length; indexRefresh++){
+					$(container).find("li").eq(indexRefresh).hide(ESASING).remove();
+					var created = $(TAG_SPAN).text($.fn.twitterReader.relativeTime(lastFechingValues[indexTwitsAdd].created_at));
+					var textFormated = $.fn.twitterReader.convertUrlToLink(lastFechingValues[indexTwitsAdd].text);
+					var newTwit = $(TAG_LI).html(textFormated + EMPTY + $(created).html()).fadeIn(ESASING).prependTo(container);
 				}
 				
-				// return data to callback function
-				if($.isFunction(options[CALLBACK_FUNCTION])){
-					options.result("refreshed");
+				// Limpia la ultima lista de datos obtenidos
+				if(numberOfNewTwits > 0){
+					firstFechingValues = lastFechingValues;
+					numberOfNewTwits = 0;
+					lastFechingValues = [];
 				}
-
+				
+				// Ejecuta el callback al refrescar los datos
+				if($.isFunction(options[CALLBACK_FUNCTION])){
+					options.result();
+				}				
 			});
 		});
 		return container;
@@ -173,7 +185,17 @@
 	$.fn.twitterReader.fetchTwits = function(options, resultFecthed) {
 		var url = TWITTER + options[TWITTER_USER] + FORMAT + "?count=" + options[NUMBER_OF_TWITS] + "&callback=?";
 		$.getJSON(url, resultFecthed);
-	};
+	};	
+				
+	/**
+	 * Reemplaza las URLs por links
+	 * @return {string} Cadena formateada
+	 */
+	$.fn.twitterReader.convertUrlToLink = function(text) {
+		return text.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/, function(text){
+			return text.link(text);
+		});
+	};				
 
 	/**
 	 * Establece el tiempo relativo del twit
@@ -181,46 +203,43 @@
 	 */
 	$.fn.twitterReader.relativeTime = function(timeValue) {
 
-		var MONTH	= 1;
+		var DAY		= 1;
+		var MONTH	= 2;
 		var YEAR 	= 5;
 		var HOUR 	= 3;
-
 		var timeValues = timeValue.split(EMPTY);
 		timeValue = timeValues[DAY] + EMPTY + timeValues[MONTH] + ", " + timeValues[YEAR] + EMPTY + timeValues[HOUR];
 		var parsedDate = Date.parse(timeValue);
 		var relativeTo = (arguments.length > ONE) ? arguments[ONE] : new Date();
 		var delta = parseInt((relativeTo.getTime() - parsedDate) / 1000);
 		delta = delta + (relativeTo.getTimezoneOffset() * 60);
-
-		var relativeTime = '';
+		
+		// Establece el tiempo de creacion del twit
+		var relativeTime = '';		
 		if (delta < 60) {
-			relativeTime = 'a minute ago';
+			relativeTime = 'hace un minuto';
 		} else if(delta < 120) {
-			relativeTime = 'couple of minutes ago';
+			relativeTime = 'hace un par de minutos';
 		} else if(delta < (45 * 60)) {
-	    		relativeTime = (parseInt(delta / 60)).toString() + ' minutes ago';
+	    	relativeTime = 'hace ' + (parseInt(delta / 60)).toString() + ' minutos';
 		} else if(delta < (90 * 60)) {
-	    		relativeTime = 'an hour ago';
+	    	relativeTime = 'hace una hora';
 	  	} else if(delta < (24 * 60 * 60)) {
-	    		relativeTime = '' + (parseInt(delta / 3600)).toString() + ' hours ago';
+	    	relativeTime = 'hace ' + (parseInt(delta / 3600)).toString() + ' horas';
 	  	} else if(delta < (48 * 60 * 60)) {
-	    		relativeTime = '1 day ago';
+	    	relativeTime = 'hace un dia';
 	  	} else {
-	    		relativeTime = (parseInt(delta / 86400)).toString() + ' days ago';
+	    	relativeTime = 'hace ' + (parseInt(delta / 86400)).toString() + ' dias';
 	  	}
 		return relativeTime;
 	}
-
-
-
 	
 	/**
 	 * Establece defaults del plugin
 	 */
 	$.fn.twitterReader.defaults = {
-		numberOfTwits: 10,
-		timeRefresh: 30000,
-		css:{coner:'30px'},
+		numberOfTwits: 	10,
+		timeRefresh: 	25000,
 		result: function(){}
 	};
 
